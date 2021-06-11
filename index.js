83b3f44ac2deb35da15fa9cb26be97c20fa3d2b3
Usama Arain <usama.arain@tmf.org.pk>
2021-06-11 21:49:36 +0500

    Changes in port

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ app.put('/image',(req,res)=>{
 // })
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log(`Express server listening on port ${process.env.PORT}`);
 });
 
 // OPERATION METHOD->RETURN
